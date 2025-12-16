c4360870e3348a3bd4827ea6e1ded0a85881bcc0
Joseph Giron <joseph.giron@ucf.edu>
2025-12-16 16:56:17 -0500

    updated view

---
+++ GoldBoxGame/Controllers/PrimaryViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoldBoxGame.Controllers
+{
+	public class PrimaryViewController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
